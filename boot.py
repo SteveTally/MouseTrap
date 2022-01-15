3473da0e0f24070ba3588c36f0f1f62a64b0bf15
Steven Tally <SteveTally@users.noreply.github.com>
2022-01-15 10:51:44 -0500

    Create boot.py

---
+++ boot.py
@@ -0,0 +1,6 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+# No code currently needs to execute on boot.  Webrepl may be an option to use in the futrue but commenting out for now.
+#import esp
+#esp.osdebug(None)
+#import webrepl
+#webrepl.start()
