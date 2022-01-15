a05be6343d8fc26ec352e71bfc6395d72df0896d
Steven Tally <SteveTally@users.noreply.github.com>
2022-01-15 10:54:36 -0500

    create config.py
    Includes local secrets and configuration used in the app

---
+++ config.py
@@ -0,0 +1,8 @@
+# local configurations
+# replace values on local device and do not sync to github repo
+from_phone = "+10000000000"
+to_phone = '+10000000000'
+ssid = "enter SSID"
+ssid_pwd = "enter wifi password"
+tw_sid = "enter Twillow SID"
+tw_token = "enter twillow token"
